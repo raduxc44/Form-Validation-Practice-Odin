adb24d83c6671aec055be375017b801c7f8904c7
raduxc44 <raduxc44@gmail.com>
2022-05-21 02:51:27 +0300

    The validationObj now fully checks the email adress field

--- script.js
+++ script.js
@@ -15,18 +15,23 @@ class validationObj {
         let emailString = email.value.toString();
         let emailArr = emailString.split('');
 
-        
-
         (function emailCheck() {
 
-            console.log('a')
             let emailMessage = document.getElementsByClassName('email-error-field')[0];
             let emailToStr = emailArr.join('')
 
-            if(emailArr.includes ('@') && emailArr.includes ('.') && (emailArr.indexOf('.') - emailArr.indexOf('@') >= 2)) {
+            if(emailArr.includes ('@') && emailArr.includes ('.') && 
+              (emailArr.indexOf('.') - emailArr.indexOf('@') >= 2) && ((emailArr.length - 1) - emailArr.indexOf('.') > 2)) {
     
                 email.classList.add('valid');
+                email.classList.remove('invalid');
                 emailMessage.innerHTML = `<p>You've entered a valid email adress!</p>`
+
+            } else {
+
+                email.classList.remove('valid');
+                email.classList.add('invalid');
+                emailMessage.innerHTML = `<p>This is not a valid email adress.</p>`
 
             }
             return emailToStr;
