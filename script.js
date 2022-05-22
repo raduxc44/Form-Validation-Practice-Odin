44555227e761d36f19367d1a9c1ac0fb85180bcf
raduxc44 <raduxc44@gmail.com>
2022-05-23 02:22:52 +0300

    The script for the email field now runs when focusing out of it

--- script.js
+++ script.js
@@ -8,37 +8,55 @@ class validationObj {
 
     inputCheck(event) {
 
-        event.preventDefault()
+        event.preventDefault();
+
+        let validCheck = document.getElementsByClassName('validCheck');
 
         //Email check
 
-        let emailString = email.value.toString();
-        let emailArr = emailString.split('');
-
         (function emailCheck() {
 
+            let emailString = email.value.toString();
+            let emailArr = emailString.split('');
             let emailMessage = document.getElementsByClassName('email-error-field')[0];
-            let emailToStr = emailArr.join('')
-
-            if(emailArr.includes ('@') && emailArr.includes ('.') && 
-              (emailArr.indexOf('.') - emailArr.indexOf('@') >= 2) && ((emailArr.length - 1) - emailArr.indexOf('.') == 3 )) {
     
+            if(email.value.length == 0) {
+                
+                // If the input field is empty, the input field returns to default
+
+                if(emailMessage) {emailMessage.remove()};
+                email.classList.remove('invalid');
+                email.classList.remove('valid');
+                validCheck[0].classList.remove('show')
+
+            }
+
+            else if (email.value.length > 0) {
+            
+                let emailVerifiesChar = emailArr.includes ('@') && emailArr.includes ('.');
+                let emailIndexDiff = emailArr.indexOf('.') - emailArr.indexOf('@');
+                let emailExtension = (emailArr.length - 1) - emailArr.indexOf('.');
+
+            if(emailVerifiesChar && emailIndexDiff >= 2 && emailExtension == 3) {
+    
+                validCheck[0].classList.add('show')
                 email.classList.add('valid');
                 email.classList.remove('invalid');
-                emailMessage.innerHTML = `<p>You've entered a valid email adress!</p>`
+                if(emailMessage){emailMessage.innerHTML = `<p>You've entered a valid email adress!</p>`};
 
             } else {
 
+                validCheck[0].classList.remove('show')
                 email.classList.remove('valid');
                 email.classList.add('invalid');
-                emailMessage.innerHTML = `<p>This is not a valid email adress.</p>`
+                if(emailMessage) {emailMessage.innerHTML = `<p>This is not a valid email adress.</p>`};
 
             }
+
+            let emailToStr = emailArr.join('')
             return emailToStr;
-        })();
-
-
- 
+        
+        }})();
     }
 }
 
@@ -46,5 +64,4 @@ let validator = new validationObj;
 let form = document.getElementById('form')
 let email = document.getElementById('email')
 let submitButt = document.getElementById('submit')
-form.addEventListener('submit', validator.inputCheck)
-
+email.addEventListener('change', validator.inputCheck)
